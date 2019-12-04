7d4af326df47fedc6795bf0d69243153fca51fa6
Lcnvdl <programando.soft@gmail.com>
2019-12-04 10:04:41 -0300

    Tests

--- test/entities.versions-manager.test.js
+++ test/entities.versions-manager.test.js
@@ -3,10 +3,44 @@ const VersionsManager = require("../src/entities/versions-manager");
 const DbDriverStub = require("./stubs/database.driver.stub");
 
 const db = new DbDriverStub();
+/** @type {VersionsManager} */
+let entity;
 
 describe("VersionsManager", () => {
-    it("constructor should work fine", () => {
-        let entity = new VersionsManager(db);
-        expect(entity).to.be.ok;
+    describe("#constructor", () => {
+        it("should work fine", () => {
+            entity = new VersionsManager(db);
+            expect(entity).to.be.ok;
+        });
+    });
+
+    describe("#getVersion", () => {
+        beforeEach(() => {
+            entity = new VersionsManager(db);
+        });
+
+        it("should generate a new version if it doesn't exists", async () => {
+            entity._versions = {
+                findOne() {
+                    return Promise.resolve(null);
+                }
+            };
+
+            let version = await entity.getVersion("users", "u1");
+            expect(version).to.be.ok;
+            expect(version.version).to.equals(0);
+        });
+
+        it("should work fine", async () => {
+            entity._versions = {
+                findOne() {
+                    return Promise.resolve({ version: 1 });
+                }
+            };
+
+            let version = await entity.getVersion("users", "u1");
+            expect(version).to.be.ok;
+            expect(version.version).to.equals(1);
+        });
     });
 });
